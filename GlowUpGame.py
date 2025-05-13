b0df316a90502cc2df2b3af8fe947db6a7df11ca
unknown <yadanar9256@gmail.com>
2025-05-13 21:09:29 +0630

    First

---
+++ GlowUpGame.py
@@ -0,0 +1,12 @@
+class player:
+    def __init__(self,name, age):
+        self.skinhealth = 5
+        self.confidence = 5
+        self.name = name
+        self.age = age
+
+    def about(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.name}")
+        print(f"Skin Health: {self.name}")
+        print(f"Self Confidence: {self.name}")
--- ex1.py
+++
@@ -1,6 +0,0 @@
-# for i in range(100):
-#     print("kiss")
-kiss = 1
-while kiss <=100:
-    print ("kiss")
-    kiss+=1
