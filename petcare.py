56217f4cad3f50fb51b931153c69f30141b2b827
unknown <yadanar9256@gmail.com>
2025-05-18 01:05:20 +0630

    Second

--- petcare.py
+++ petcare.py
@@ -2,6 +2,45 @@
 class Pet:
     def __init__(self,name):
         self.name = name
+        self.food = 0
+        self.love = 0
+
+    def introduce(self):
+        print(f"Hello. I am {self.name} and your virtual pet")
+
+    def feed(self):
+        feed= input("You can feed me 1. Pizza🍕 2. Sushi🍣 3. Croissant🥐 and 4. Bubble Tea🧋")
+        if feed == 1:
+            print("Yayyyy.. You feed me pizza🍕")
+            self.food += 30
+        elif feed == 2:
+            print("Yayyyy.. You feed me sushi🍣")
+            self.food += 30
+        elif feed == 3:
+            print("Yayyyy.. You feed me Croissant🥐")
+            self.food += 20
+        else: 
+            print("Yayyyy.. Bubble tea is delicious🧋")
+            self.food += 15
+    
+    def pet(self):
+        for i in range (5):
+            pet = input("Will you pet me hooman🥺 y/n").strip().lower()
+            if pet == 'y':
+                print ("Happi❤️")
+                self.love += 10
+            else:
+                print("It's ok hooman🥺")
+    
+    def about(self):
+        print(f"Name: {self.name}")
+        print(f"Hunger level: {self.food}%")
+        print(f"Love: {self.love}%")
+        if self.skinhealth <= 15:
+            print ("You have to learn more about skincare")
+        else:
+            print ("You are doing great")
+
 print("Here's your virtual pet")
 pet = input("1. Penguin 2. Dog 3. Cat")
 if pet == 1:
