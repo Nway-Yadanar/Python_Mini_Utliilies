6c5782ee21d2800ecfc8124562c3bf501e2e4a10
unknown <yadanar9256@gmail.com>
2025-05-18 15:08:43 +0630

    Fourth

--- petcare.py
+++ petcare.py
@@ -4,20 +4,24 @@ class Pet:
         self.name = name
         self.food = 0
         self.love = 0
+        self.happiness = 0 #for happiness meter
 
     def introduce(self):
         print(f"Hello. I am {self.name} and your virtual pet")
 
     def feed(self):
-        for i in range(5):
-            feed= input("You can feed me 1. Pizza🍕 2. Sushi🍣 3. Croissant🥐 and 4. Bubble Tea🧋")
-            if feed == 1:
+        feed = input("Will you feed me hooman y/n:  ").strip() .lower()
+        if feed == 'y':
+        
+            feed= input("You can feed me 1. Pizza🍕 2. Sushi🍣 3. Croissant🥐 and 4. Bubble Tea🧋").strip()
+            if feed == '1':
                 print("Yayyyy.. You feed me pizza🍕")
                 self.food += 30
-            elif feed == 2:
+
+            elif feed == '2':
                 print("Yayyyy.. You feed me sushi🍣")
                 self.food += 30
-            elif feed == 3:
+            elif feed == '3':
                 print("Yayyyy.. You feed me Croissant🥐")
                 self.food += 20
             else: 
@@ -25,7 +29,7 @@ class Pet:
                 self.food += 15
     
     def pet(self):
-        for i in range (5):
+        
             pet = input("Will you pet me hooman🥺 y/n").strip().lower()
             if pet == 'y':
                 print ("Happi❤️")
@@ -44,11 +48,51 @@ class Pet:
         else:
             print("Full energy")
 
+class Dog(Pet):
+    def make_sound(self):
+        print("Woof")
+    
+    def fetch(self):
+        # need to add more ball
+        print("fetching the ball....")
+
+
 class Cat(Pet):
     def make_sound(self):
         print("Meow")
+    
+    def nap(self):
+        print("Purrrr....😸😴")
+
+class Penguin(Pet):
+    def slide(self):
+        print("🐧Wheee! I'm sliding on the ice⛸️")
+   
 
 print("Here's your virtual pet options. Choose 1,2 or 3.")
 pet = input("1. Penguin 2. Dog 3. Cat :  ")
+if pet == '1':
+    name = input("What would you like to name your penguin? ")
+    manchot = Penguin(name) 
+    manchot.introduce()
+    manchot.feed()       
+    manchot.pet()
+    manchot.about()
+
+if pet == '2':
+    name = input("What would you like to name your dog? ")
+    chien = Dog(name) 
+    chien.introduce()
+    chien.make_sound()
+    chien.feed()
+    chien.pet()
+    chien.about()
+
 if pet == '3':
-    name = input("What would you like to name your cat")
+    name = input("What would you like to name your cat? ")
+    chat = Cat(name) 
+    chat.introduce()
+    chat.make_sound()
+    chat.feed()
+    chat.pet()
+    chat.about()
